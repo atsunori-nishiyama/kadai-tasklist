782edaa56cdc69c6fdfd663235a9b6761b08d79b
atsunori.nishiyama <nishiyama1980@icloud.com>
2019-08-12 06:05:05 +0000

    user login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+   
+   include SessionsHelper
+    
+  private
+    
+  def require_user_logged_in
+    unless logged_in?
+    redirect_to sessions_new_url
+    end
+  end
+    
 end
